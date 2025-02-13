3d369fd1f5d2ef8d08771809f30b6e97ee7b5642
Dave Victoria <davidvictoria64@gmail.com>
2025-02-13 15:07:57 -0600

    ✨ feat: Add og image for SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,25 @@
 import "@/app/globals.css";
 
 export const metadata = {
-  title: "Dave Victoria",
-  description: "Espacio para compartir mis conocimientos y experiencias",
+  title: "David Victoria",
+  description: "Aprendizajes y experiencias de un ingeniero de software",
+  openGraph: {
+    title: "David Victoria",
+    description: "Aprendizajes y experiencias de un ingeniero de software",
+    url: "https://www.davidvictoria.io",
+    type: "website",
+    images: [
+      {
+        url: "/images/og-image.jpg",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "David Victoria",
+    description: "Aprendizajes y experiencias de un ingeniero de software",
+    images: ["/images/og-image.jpg"],
+  },
 };
 
 export default function RootLayout({ children }) {
